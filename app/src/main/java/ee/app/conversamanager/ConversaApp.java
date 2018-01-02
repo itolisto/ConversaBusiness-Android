268983efdb9b76904513588b7a3c030876637037
Edgar Mauricio Gómez Flores <itolisto@gmail.com>
2018-01-01 21:32:25 -0600

    Change Ably compiled AAR version
    Changing Ably AAR to the most recent version working with push notifications.

--- app/src/main/java/ee/app/conversamanager/ConversaApp.java
+++ app/src/main/java/ee/app/conversamanager/ConversaApp.java
@@ -26,6 +26,7 @@ package ee.app.conversamanager;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.StrictMode;
 import android.support.multidex.MultiDexApplication;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatDelegate;
@@ -105,7 +106,7 @@ public class ConversaApp extends MultiDexApplication {
 					//localhost
 					.applicationId("b15c83")
 					.clientKey(null)
-					.server("http://192.168.1.8:1337/parse/") // The trailing slash is important.
+					.server("http://10.0.3.2:1337/parse/") // The trailing slash is important.
 					.build()
 			);
 		} else {
@@ -120,14 +121,14 @@ public class ConversaApp extends MultiDexApplication {
 
 	private void initializeDeveloperBuild() {
 		if (BuildConfig.DEV_BUILD) {
-//			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-//					.detectAll()
-//					.penaltyLog()
-//					.build());
-//			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-//					.detectAll()
-//					.penaltyLog()
-//					.build());
+			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+					.detectAll()
+					.penaltyLog()
+					.build());
+			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+					.detectAll()
+					.penaltyLog()
+					.build());
 		}
 	}
 
@@ -172,8 +173,7 @@ public class ConversaApp extends MultiDexApplication {
 	}
 
 	private void initializeEventBus() {
-		EventBus
-				.builder()
+		EventBus.builder()
 				.addIndex(new MyEventBusIndex())
 				.throwSubscriberException(BuildConfig.DEV_BUILD).installDefaultEventBus();
 	}
